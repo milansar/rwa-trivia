63104ed5b8fbdfd4da75099a8dc11b31a48f9d84
Milan Saraiya <milan@ashutec.com>
2020-08-13 14:43:40 +0530

    integrated @nativescript/iqkeyboardmanager instead of nativescript-iqkeyboardmanager

--- references.d.ts
+++ references.d.ts
@@ -1,4 +1,4 @@
 /// <reference path="./node_modules/@nativescript/types/ios.d.ts" />
 /// <reference path="./node_modules/@nativescript/types/android.d.ts" />
-/// <reference path="./node_modules/nativescript-iqkeyboardmanager/index.d.ts" />
+/// <reference path="./node_modules/@nativescript/iqkeyboardmanager/index.d.ts" />
 /// <reference path="./node_modules/@nativescript/core/index.d.ts" />
